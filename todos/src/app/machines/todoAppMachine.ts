9e5ddef79aaa73176ab13959f9782464ea7a7d42
Raymond Young <raymond@philo.com>
2025-05-12 14:39:07 -0700

    use actions in state machine to log todos when loaded

--- todos/src/app/machines/todoAppMachine.ts
+++ todos/src/app/machines/todoAppMachine.ts
@@ -4,15 +4,29 @@ export const todosMachine = createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxDXU+qrANoAMAXUSgADj2IAXYqlIiQAD0QBaAMwBOACwV+AVgAcu9aoDsugDQgAnioCM-CgCYTqzad0BfD5eZY8RMkoOelJGXzYAMVxiABtMYIYuFgFhJBBxWCkZOTSlBGVHXVUnTQA2fmMzSxt821tSijqTW30zL28QUnQ4eV8cAhJyeQys2Xk85TLdChNHR1LdUsLqlUd+Yv1S5tbPDr7-QaDaELDueDSR6THclU1bExm5haWLazt7ilUtlra97n6AuQKOF4sdIMMJFccqAJqo9CVHD9XjVWhR1Iidl4fP8DoFqMdEiCAKIAJxJqBJ4IukOy4zsjlUK3yqmK6lKmPaQA */
   id: "Todo Machine",
   initial: "Loading Todos",
+  types: {
+    events: {} as { type: 'Load Todos'; todos: string[] } | { type: 'Fail Loading Todos'; errorMessage: string },
+    // typegen: {} as import ('./todoAppMachine.typegen').Typegen0
+  },
   states: {
     "Loading Todos": {
       on: {
-        "Load Todos": "Todos Loaded",
-        "Fail Loading Todos": "Loading Todos Errored"
-      }
+        "Load Todos": {
+          target: "Todos Loaded",
+          actions: 'consoleLogTodos',
+        },
+        "Fail Loading Todos": {
+          target: "Loading Todos Errored",
+        }
+      },
     },
-
     "Todos Loaded": {},
     "Loading Todos Errored": {}
+  }
+}, {
+  actions: {
+    consoleLogTodos: ({event}) => {
+      console.log(event?.todos)
+    }
   }
 })
--- todos/src/app/page.tsx
+++ todos/src/app/page.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import { useMachine } from "@xstate/react";
+import { useActor } from "@xstate/react";
 import { todosMachine } from "./machines/todoAppMachine";
 
 export default function Home() {
-  const [state, send] = useMachine(todosMachine)
+  const [state, send] = useActor(todosMachine)
   return (
     <div>
       <pre>{JSON.stringify(state.value, null, 2)}</pre>
       <div>
-        <button onClick={() => send({ type: 'Load Todos' })}>Load Todos</button>
+        <button onClick={() => send({ type: 'Load Todos', todos: ['Take bins out', 'Buy groceries'] })}>Load Todos</button>
       </div>
       <div>
-        <button onClick={() => send({ type: 'Fail Loading Todos' })}>Fail Loading Todos</button>
+        <button onClick={() => send({ type: 'Fail Loading Todos', errorMessage: 'Oh no!' })}>Fail Loading Todos</button>
       </div>
     </div>
   );
