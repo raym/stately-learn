98a1c1cfd1f41de33fbd6cf3060bf469c2168110
Raymond Young <raymond@philo.com>
2025-05-13 11:31:21 -0700

    add a guard for when fetching todos returns an empty array

--- todos/src/app/machines/todoAppMachine.ts
+++ todos/src/app/machines/todoAppMachine.ts
@@ -1,9 +1,9 @@
 import { assign, createMachine, fromPromise } from "xstate";
 
-const todos = new Set<string>(['Take bins out', 'Buy groceries'])
+const todos = new Set<string>()//['Take bins out', 'Buy groceries'])
 
 export const todosMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogC0AJi0BmCs4CMADk8AWAOxOtbw8nJwAaEABPO08tVzctADZ7X197BJSnNwBWAF8c8KEcAhJOCXpSRiE2MAAnGtQaiiMAGxUAMwaAWwpCkRLxWnLKmTkFJXNNXX1LE1gzNVJLGwRbbwBONYosrSz7NzWPDezvZPCohCdfCn8Pey97XPyQXuKxHpHMMshWAGEasBUYEw5AA7tMkCBZvMLBDlm4Et4thtfOssgl4VotPZ7Gc7FcDgj7Jdsd4kk4DnkCjIiqJOFVPoNvgARMDNMDKIHKGTg4ymCZLRCOWJuJxpBJZEXpXzirK4lYBFzxNzeFXJJwnUmU57UvpvP4A1QVYFgEGYLkYCgAZUIqBBUg6NU6mDIRgArspWAAxLrO0hu5SYIi4CqQHmQvkLAUrNZuCjrLQeYIqzzKrKyyKC9IUZIJQKXXZrNJuXxal60yj6lRSUFmmRWm12o0Op0u92sS2ugBGnTMYah-NhmfsFCl0vsa0y-kScvsfmzIqy3gCiTShwSpZ1r04lcNjBr5tQVtw3CkB-Y6koCgE7wwNP6FB31ZNtYtluPp5k8lIvHGC30fYjGFQGWPxYxCLIp0XDwJRxDMEHHWNfF2LIvDWBJ0OcJwN1vXVt3+KsjX3Os3xPI0z1qepGhadouhvYQtwrfDd2NU0DyPUjGAPL8fyrdR-10GZAMWQcEG8NMtnHUUNnHbwtAOXw5SXBIthQhIQg8dEAhjbD6PLCgWTZZizw4S9v34QRNz0gz2Q-DBuMUXjJj0ASIX7SMRMuWIdnhW4QhSeE0TlBwThHbFJSXA5yXQnS7zeayjJkVgKIaJpWmUZs6Nizh4ts1B7N-Piphc3k5gHYDECQrIKFuZJAg8UkUSxIKfJHFdEi0Nx9g67wYtwyh4s5alkv+CBWCsWBlEBChcDaDkagACgeTEAEpWDLe8BpfLBhtDYrw1K9zyoQXNNl8eIvA8aVvLTILvFjfZghQtFSXFW4eqeda4tZdlBtvHbRqZYhYB7WBYEwYaAIOoDrEQLI1iuMSEhjRwnCSdIPDlS5qrQlVUixPwZLyJ5SHQOBLE+8hBKh4SjtsRcrm2XYHqORcwjg2xyViXNlUyEUUfhx4qRwhjqEGKQqip6EaZhlZ1kQ+q0l8NZZOLIkEma3YKCcFC1jktSOonQtepF+kvggSWyplnxXHhtTRVXM7sg1jwtnVRIsm1tFEjcY29LKcWPgAUTqBpIAtw6ZdWW4taxOSghZpc5WLKrfD18kkOxA37F9+9H0I58D3D6HllsEVNnjRN1TurxEaT7Jrlx4I0PkhN3qF3Tc6Yp9WOIht7R9VtlCL6Xll8F2sQ2Rc7ru9q2fOFHs1xws0kyTEPBzvUu-znvX3fMiZGHqNVxttCQnQjOzsxpctbEhM0M8DP1w+yyNu+hKMEPkSIMRZd0O1+rdgY3ZuOZSCQvDqnquSNMyssLP2FlZN+v1tohxGp-I6HhEiuHqo7JWU5laY2zOiZWyIDi2yXI8PIQA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogC0AJgAsARgoBWZ-YDMbr84BsAOwAHMH2YQA0IACedvbOXu4AnMGO9klJzs7Bbm4AvnlRQjgEJJwS9KSMQmxgAE51qHUURgA2KgBmTQC2FMUiZeK0ldUycgpK5pq6+pYmsGZqpJY2CLaOGe5abvEpGc5uaYFRsQhegRSBXmHBnvmFIP2lYn1jmBWQrADCdWAqYJhyAB3WZIEDzRYWMGrAKOZJJQIbNz+AJaLT2ewnOwXFL+NLnDGOfzeFIFIoyEqiTg1d7DT4AETArTAygByhkoOMpimK0Q9jRFAS9n8-g8XiCgVFbixay8WkSzi0zkcKvsVzSRLJjwpAxePz+qiqgLAQMw7IwFAAyoRUECpF06t1MGQjABXZSsABiPWdpDdykwRFwVUgnPB3KWvLWmQoGy0oS8KuyytyMuFFzV-i0jnOOySwucgS1TyplH1KikwLNMitNrtRodTpd7tYltdACNumYwxCedC+cSKBLJek-FctP4044MwlDnKJ8Lgkl-MWdc9OOXDYwq+bUFbcNwpLv2OpKAoBK8MJTBhRN5WTdWLZaD0eZPJSLxJkt9D2I1DQKs06uF4PjjocOSeGmMaBDsbi3MuIryl4q5XrqG6-BWRo7jWz6Hkax71I0zRtJ0PSXsI65lhhW7Gqau77nhjC7u+n4VuoP66HMf7LP2CCOLk7ijuk+ZJI4WgpMcMSIDm-juHB-ggcEKJypkKEUaWFCMsyNHHhwZ4fvwghrhpWksq+GAsYobHTHonFgr2ka8ecWgUNsARhCBgSBAEyIyg4aRDhiCR4l4KShSKanXi8pk6TIrCEU0LTtMojbkVFnAxeZqCWV+7EzHZXILH2AGIDBbgUGEarZqkQRiZiUlrO5Q4LhOSrOEkSqOJFaGUDFbIUglvwQKwViwMo-wULgHSsnUAAU9huGiACUrAljefWPlgg2hgV4ZFY5JUIFmSSXIqtzBJKbmpg16yuO1wQ+DkeIinBTjdZRmlMiy-VXttw30sQsBdrAsCYINv77f+1iIG4CKxsimT8uK6bBDK5wVcuKqBPyTjY6J70aRUUg1CenDnkZqEfUTRo1Dl1k-s4Bj2dxUYOPyFXiqk8aBJk+bXacthZIk2NKaKRLxMESoFA8pDoHAlhrWIXGQzxh22IcFyLTs937HOfmhS5WbKn4Qrygi9zkpThPDMTYzK5CqvQ2sGyuCEaRBKJSrY+KfkHPYFCPR1y5yu1oX2ATN40h8ED28VTvZHC4mSiBGL+BdWTSjdBzBO4iYTr4uRZiiEcvNTowsJgACiDRNJAscHU76xhAH6LiY4S6ZHrDWFuVgTiQp5sYkq+Yl+hBr3nRMj11DqyC6FsYdQmSa3Pxk7dwclxYw9y4SfGXUPIrY+YduD70datr2j6zbKNPjurCErnCYcLguK1XhpvKFBONO+bCn4aLBFHlRceWFT44RfPhKezMVZRkXK5DIClxRBB2N5NGOYA78XjMubIMFhRAM+tpLKt8oxuERC3CcCk4L8XCH5dIsk04JHbjmJIuRRLIQPsZdaX1WSbTBjXIaxDeKS1knvbyhYEQqUcGjL+KJRIZB5kuSUOZ7gFCAA */
   id: "Todo Machine",
   initial: "Loading Todos",
   types: {
@@ -52,10 +52,14 @@ export const todosMachine = createMachine({
       invoke: {
         src: 'loadTodos',
 
-        onDone: {
+        onDone: [{
           target: 'Todos Loaded',
           actions: 'assignTodosToContext',
-        },
+          guard: "hasTodos"
+        }, {
+          target: "Creating new todo",
+          reenter: true
+        }],
 
         onError: {
           target: "Loading Todos Errored",
@@ -159,8 +163,11 @@ export const todosMachine = createMachine({
       return todos.add(context.input.createNewTodoFormInput)
     }),
     deleteTodo: fromPromise(async (context) => {
-      throw new Error('Oh no, could not delete todo!')
-      // return todos.delete(context.input.todo)
+      // throw new Error('Oh no, could not delete todo!')
+      return todos.delete(context.input.todo)
     })
-  }
+  },
+  guards: {
+    hasTodos: ({event}) => event.output.length > 0
+  },
 })
